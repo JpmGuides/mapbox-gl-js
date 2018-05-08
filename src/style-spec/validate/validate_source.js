ae83c7c4c0e8cc0fe7f8eb6e08b6f27753e6948a
Julien Pilet <julien.pilet@gmail.com>
2018-05-08 10:59:31 +0200

    add support for mbtiles source type

--- src/style-spec/validate/validate_source.js
+++ src/style-spec/validate/validate_source.js
@@ -18,6 +18,7 @@ export default function validateSource(options) {
     let errors = [];
 
     switch (type) {
+    case 'mbtiles':
     case 'vector':
     case 'raster':
     case 'raster-dem':
@@ -77,7 +78,7 @@ export default function validateSource(options) {
         return validateEnum({
             key: `${key}.type`,
             value: value.type,
-            valueSpec: {values: ['vector', 'raster', 'raster-dem', 'geojson', 'video', 'image', 'canvas']},
+            valueSpec: {values: ['mbtiles', 'vector', 'raster', 'raster-dem', 'geojson', 'video', 'image', 'canvas']},
             style: style,
             styleSpec: styleSpec
         });
